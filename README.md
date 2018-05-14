516ce4cc29ee8c902e66aadb8eb970a18aa5ab4e
yfsoftcom <yfsoftcom@126.com>
2018-05-14 10:05:40 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+## FPM-PLUGIN-ADMIN
+用于显示后台管理页面的插件
+
+### Install
+```bash
+yarn add fpm-plugin-admin
+```
+
+### Useage
+
+default user
+admin
+741235896
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash'
+import path from 'path'
+import Views from 'koa-views'
+import Session from 'koa-session2'
+import Static from 'koa-static'
+import AdminRouter from './admin.js'
+
+import session from './session.js'
+
+const LOCAL = path.join(__dirname, '../')
+export default {
+  bind: (fpm) => {
+    // Run When Server Init
+    fpm.registerAction('INIT', () => {
+      console.log(fpm.getVersion())
+      const c = fpm.getConfig()
+      console.log('Run Init Actions: local path: ', LOCAL)
+    })
+
+    fpm.registerAction('ADMIN', () => {
+      let admin = fpm.createRouter()
+      admin = AdminRouter(admin)
+      
+      const app = fpm.app
+      app.use(Views(path.join(LOCAL, 'views'), {
+        extension: 'html',
+        map: { html: 'nunjucks' },
+      }))
+      app.use(Static(path.join(LOCAL, 'public')))
+      app.use(Session({ key: 'fpm-server-admin' }))
+      app.use(session)
+  
+      fpm.bindRouter(admin)
+    })
+
+  }
+}
